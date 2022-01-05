33efebd6bac8e32dcac423ab0eff6ca25835ceac
Merve Bayar <76745713+bayarm2@users.noreply.github.com>
2022-01-05 16:54:13 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,5 +32,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
     #
-    from .group10 import group10
+from .group10 import group10
     return group10(iface)
